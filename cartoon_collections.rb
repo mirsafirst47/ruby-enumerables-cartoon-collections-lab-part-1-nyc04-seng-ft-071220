692cba620fa9f966d228434aeb99e609a4b46069
Samir Triande <habib.triande@gmail.com>
2020-07-21 10:36:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   list = []
-  array.each do |char, index|
+  array.each_with_index do |char, index|
     list[char] = index
   end
   list
