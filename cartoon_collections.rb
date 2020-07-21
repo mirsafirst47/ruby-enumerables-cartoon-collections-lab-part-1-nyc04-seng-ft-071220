4ac41652593d46ca152064d41fc65a2816d6fb71
Samir Triande <habib.triande@gmail.com>
2020-07-21 10:47:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |char, index|
-    puts "#{index + 1}. #{char}"
+    puts "#{index + 1}. #{char}"# puts the index first + 1 to start the list from 1 and not zero, then the element corresponding to that index.
   end
 end
